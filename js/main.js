89d574283ef947a562feb4f95ec589ad3fc48463
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-06-27 13:40:08 +0700

    feat: add burger menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
         }
     };
 }());
+
+// Burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close')
+    burgerItem.addEventListener('click', ()=> {
+        menu.classList.add('header__nav_active');
+    })
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    })
+}());
